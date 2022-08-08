22be15e3cf8fc8bce8539f2e0f388dfc58ece68c
priyanshuyadavmagic <100867711+priyanshuyadavmagic@users.noreply.github.com>
2022-08-08 10:05:51 +0530

    Update startApplication.sh

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user
-sudo rm /var/www/html/index.html
-sudo cp index.html /var/www/html/index.html
+# cd /home/ec2-user
+# sudo rm /var/www/html/index.html
+# sudo cp index.html /var/www/html/index.html
+echo "hello java"
